f5f0a7a3b4463f79876884395eda540c2ad45c7c
Daniel Boorn <daniel.boorn@gmail.com>
2024-04-27 09:38:53 -0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# btcsupervaluemap
-TradingView Seed of BTC Super Value Map
+# BTC Super Value Map Seed for Trading View
+
+CC BY-SA 4.0 DEED
+Attribution-ShareAlike 4.0 International
+License: https://creativecommons.org/licenses/by-sa/4.0/
+
+BTC Super Value Map Seed for Trading View
+
+Seed for TradingView based on custom formulas developed by Daniel Boorn with Glassnode data.
---
+++ convert.php
@@ -0,0 +1,59 @@
+<?php
+
+
+class Util
+{
+    public static function keyName($key)
+    {
+        return urlencode(str_replace([':', ' '], ['', '_'], $key));
+    }
+
+    public static function import($filename)
+    {
+        $buffer = file_get_contents($filename);
+        return json_decode($buffer, true);
+    }
+
+    public static function getSymInfo(array $data)
+    {
+        $info = ['symbol' => [], 'pricescale' => [], 'description' => []];
+        foreach (array_keys($data) as $key) {
+            $info['symbol'][] = static::keyName($key);
+            $info['pricescale'][] = 10000000000000000000000;
+            $info['description'][] = static::keyName($key);
+        }
+        return $info;
+    }
+
+    public static function toRepoFiles(array $data)
+    {
+        file_put_contents('symbol_info/seed_crypto_santiment.json', json_encode(static::getSymInfo($data)));
+        foreach ($data as $key => $value) {
+            $keyName = static::keyName($key);
+
+            $f = fopen("data/{$keyName}.csv", 'r+');
+            foreach ($value as $row) {
+                fputcsv($f, [
+                    date('Ymd\T', strtotime($row['t'])),
+                    $row['v'],
+                    $row['v'],
+                    $row['v'],
+                    $row['v'],
+                    0
+                ]);
+            }
+            fclose($f);
+        }
+    }
+
+    public static function gen($filename)
+    {
+        $data = static::import($filename);
+        static::toRepoFiles($data);
+    }
+
+}
+
+Util::gen('input.json');
+
+die("\n\nDone!\n\n");
